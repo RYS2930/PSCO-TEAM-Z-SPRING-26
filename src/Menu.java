937dc241abd6d335309e596955dd6c43d66933b9
Ege-Eren <te203@sussex.ac.uk>
2026-02-13 18:04:10 +0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+        UserProfile user = UserManager.loadOrCreateUser(username);
 
         while (true) {
             System.out.println("\n=== Dice Game Menu ===");
@@ -10,22 +15,45 @@ public class Menu {
             System.out.println("2. Dice Grid Puzzle");
             System.out.println("3. Dice Codebreaker");
             System.out.println("4. Dice Battle");
-            System.out.println("5. Exit");
+            System.out.println("5. View Player Stats");
+            System.out.println("0. Exit");
             System.out.print("Choose: ");
 
             int choice = sc.nextInt();
 
             switch (choice) {
-                case 1 -> new DicePatternGame().play();
-                case 2 -> new DiceGridGame().play();
-                case 3 -> new DiceCodebreakerGame().play();
-                case 4 -> new DiceBattle().play();
-                case 5 -> {
+                case 1 -> {
+                    DicePatternGame g = new DicePatternGame();
+                    g.play();
+                    user.updatePatternScore(g.calculateScore());
+                    user.incrementGamesPlayed();
+                }
+                case 2 -> {
+                    DiceGridGame g = new DiceGridGame();
+                    g.play();
+                    user.updateGridScore(g.calculateScore());
+                    user.incrementGamesPlayed();
+                }
+                case 3 -> {
+                    DiceCodebreakerGame g = new DiceCodebreakerGame();
+                    g.play();
+                    user.updateCodebreakerScore(10); // başarıya göre sabit skor
+                    user.incrementGamesPlayed();
+                }
+                case 4 -> {
+                    DiceBattle g = new DiceBattle();
+                    g.play();
+                    user.updateBattleScore(20); // kazanma bazlı skor
+                    user.incrementGamesPlayed();
+                }
+                case 5 -> System.out.println(user);
+                case 0 -> {
+                    UserManager.saveUser(user);
                     System.out.println("Bye!");
                     return;
                 }
-                default -> System.out.println("Invalid choice.");
             }
+            UserManager.saveUser(user);
         }
     }
 }
