8394658dbaf1a96a2f3d4a96782c21753bfdfaeb
thaisonduong <td382@sussex.ac.uk>
2026-02-26 13:40:51 +0000

    Update Dice Pattern Game

--- src/DicePatternGame.java
+++ src/DicePatternGame.java
@@ -1,29 +1,68 @@
 import java.util.*;
 
 public class DicePatternGame {
-    private int[] dice = new int[5];
-    private Random rand = new Random();
-    private Scanner sc = new Scanner(System.in);
+    private final int[] dice = new int[5];
+    private final Random rand = new Random();
+    private final Scanner sc;
+
+    
+    public DicePatternGame() {
+        this(new Scanner(System.in));
+    }
+
+    
+    public DicePatternGame(Scanner sc) {
+        this.sc = sc;
+    }
 
     public void play() {
         rollAll();
         System.out.println("Initial roll: " + Arrays.toString(dice));
 
-        for (int i = 0; i < 2; i++) {
+        for (int round = 0; round < 2; round++) {
             System.out.print("Re-roll? (y/n): ");
-            if (!sc.next().equalsIgnoreCase("y")) break;
+            String ans = sc.next();
+            if (!ans.equalsIgnoreCase("y")) break;
 
             System.out.print("Enter indices to reroll (0-4), separated by space, end with -1: ");
+
+            boolean[] rerolled = new boolean[5]; 
             while (true) {
-                int idx = sc.nextInt();
+                Integer idx = readIntSafe();
+                if (idx == null) {
+                    System.out.print("Invalid input. Enter 0-4 or -1 to stop: ");
+                    continue;
+                }
+
                 if (idx == -1) break;
-                if (idx >= 0 && idx < 5) dice[idx] = rollDie();
+
+                if (idx < 0 || idx > 4) {
+                    System.out.print("Out of range! Enter 0-4 or -1 to stop: ");
+                    continue;
+                }
+
+                if (rerolled[idx]) {
+                    System.out.print("Index " + idx + " already rerolled this round. Choose another or -1: ");
+                    continue;
+                }
+
+                dice[idx] = rollDie();
+                rerolled[idx] = true;
             }
+
             System.out.println("Current dice: " + Arrays.toString(dice));
         }
 
         int score = calculateScore();
         System.out.println("Final score: " + score);
+    }
+
+    private Integer readIntSafe() {
+        if (!sc.hasNextInt()) {
+            sc.next(); 
+            return null;
+        }
+        return sc.nextInt();
     }
 
     private void rollAll() {
@@ -38,8 +77,9 @@ public class DicePatternGame {
         Map<Integer, Integer> freq = new HashMap<>();
         for (int d : dice) freq.put(d, freq.getOrDefault(d, 0) + 1);
 
-        boolean straight = freq.keySet().containsAll(Arrays.asList(1,2,3,4,5)) ||
-                freq.keySet().containsAll(Arrays.asList(2,3,4,5,6));
+        boolean straight =
+                freq.keySet().containsAll(Arrays.asList(1, 2, 3, 4, 5)) ||
+                freq.keySet().containsAll(Arrays.asList(2, 3, 4, 5, 6));
 
         if (freq.containsValue(5)) return 50;
         if (freq.containsValue(4)) return 40;
